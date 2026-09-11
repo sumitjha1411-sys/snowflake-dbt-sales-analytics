008070bb12a8c596c5cce1b035cedd3518fbc299
Sumit Jha <sumitjha14111985@gmail.com>
2026-09-11 20:08:34 +0000

    commit by sumit

---
+++ models/staging/product.sql
@@ -0,0 +1,9 @@
+SELECT
+      booking_id AS property_id,
+      INITCAP(product_sub_category) AS property_type,
+      INITCAP(product_category) AS property_category,
+      INITCAP(product_name) AS property_subcategory,
+      ROUND(product_price,2) AS property_price,
+      ROUND(product_cost,2) AS property_cost,
+      TO_DATE(launch_date, 'DD-MM-YYYY') AS property_launch_date
+FROM {{ source('stg_source', 'product') }}
