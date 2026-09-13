de81bdd11e144ffe231e51cfbec63d7fc92fe502
Sumit Jha <sumitjha14111985@gmail.com>
2026-09-13 12:00:19 +0000

    commit by sumit

--- models/calculate_new_revenue.sql
+++ models/calculate_new_revenue.sql
@@ -1,7 +1,10 @@
+-- In this model "calculate_new_revenue" is a macro and passing arguments are 'revenue' and 'multiplier(1.10)'-dynamic
+-- multiplier could be any number
 
 SELECT 
-    CUSTOMER_ID,
-    FULL_NAME,
-    REVENUE,
-    {{calculate_new_revenue('revenue')}} AS new_revenue
+    customer_id,
+    full_name,
+    revenue,
+    ROUND({{calculate_new_revenue('revenue',1.10)}},2) AS new_revenue
 FROM {{ ref('fact_bookings') }}
+
